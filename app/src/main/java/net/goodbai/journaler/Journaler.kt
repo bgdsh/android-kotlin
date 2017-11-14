6365cd857122994914f87a1dbd848f3f2b050377
bgd_sh <bgd_sh@hotmail.com>
2017-11-14 15:45:29 +0800

    ch02 done!

--- app/src/main/java/net/goodbai/journaler/Journaler.kt
+++ app/src/main/java/net/goodbai/journaler/Journaler.kt
@@ -2,15 +2,28 @@ package net.goodbai.journaler
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 
 class Journaler : Application() {
     companion object {
         var ctx: Context? = null
+        val tag = "Journaler"
     }
 
     override fun onCreate() {
         super.onCreate()
         ctx = this.applicationContext
+        Log.v(tag, "[ ON CREATE ]")
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        Log.w(tag, "[ ON LOW MEMORY]")
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        Log.d(tag, "[ ON TRIM MEMORY ]: $level")
     }
 }
--- app/src/main/java/net/goodbai/journaler/activity/MainActivity.kt
+++ app/src/main/java/net/goodbai/journaler/activity/MainActivity.kt
@@ -3,11 +3,58 @@ package net.goodbai.journaler.activity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import net.goodbai.journaler.Journaler
 import net.goodbai.journaler.R
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    val tag = Journaler.tag
+    override fun onCreate(
+            savedInstanceState: Bundle?,
+            persistentState: PersistableBundle?
+    ) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+        Log.v(tag, "[ ON CREATE ]")
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        Log.v(tag, "[ ON POST CREATE ]")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(tag, "[ ON RESTART ]")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(tag, "[ ON START ]")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(tag, "[ ON RESUME ]")
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        Log.v(tag, "[ ON POST RESUME ]")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(tag, "[ ON PAUSE ]")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(tag, "[ ON STOP ]")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(tag, "[ ON DESTROY ]")
     }
 }
